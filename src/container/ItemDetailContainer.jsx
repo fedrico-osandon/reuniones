781a54b56a60147d085085762d3c4fa0fd286d4d
suasneiber <57971818+suasneiber@users.noreply.github.com>
2021-05-13 23:47:58 -0300

    Update ItemDetailContainer.jsx
    conexion con base dedatos de reuniones

--- src/container/ItemDetailContainer.jsx
+++ src/container/ItemDetailContainer.jsx
@@ -1,17 +1,31 @@
 import {useState, useEffect} from 'react'
-import getItem from '../GetItem'
+//import getItem from '../GetItem'
 import ItemList from '../ItemList'
+import {getFirestore} from '../firebase'
+import { useParams } from 'react-router-dom'
+
 const ItemDetailContainer =() =>{
 
     const [data, setData]=useState([])
 
+    const [data, setData]=useState([])
+    const {idReunion = undefined} = useParams()
+
     useEffect(() => {
-        getItem().then(res=>{
-            setData(res)
-            console.log("data", data)
+        const db = getFirestore()
+        const itemCollection = db.collection('reuniones')
+
+        const query = idReunion ? itemCollection.where('idReunion', '==', idReunion) : itemCollection
+
+        query.get().then((querySnap) => {
+            if(querySnap.size === 0){
+                console.log("no existen reuniones disponibles");
+            }
+
+            setData(querySnap.docs.map( doc => ({...doc.data(), id: doc.id})));
         })
         
-    })
+    }, [idReunion])
 
     return(
         <ItemList data={data}/>
