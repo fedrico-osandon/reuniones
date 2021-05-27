bf527f213d330376a4ff8e4090b0a32f904a7229
suasneiber <57971818+suasneiber@users.noreply.github.com>
2021-05-26 21:25:39 -0300

    container

--- src/container/ItemDetailContainer.jsx
+++ src/container/ItemDetailContainer.jsx
@@ -2,37 +2,45 @@ import {useState, useEffect} from 'react'
 //import getItem from '../GetItem'
 import ItemList from '../ItemList'
 import {getFirestore} from '../firebase'
-import {Container} from 'react-bootstrap'
-import '../Card.css'
+import NavBar from '../Navbar'
+//import { useParams } from 'react-router-dom'
+
 const ItemDetailContainer =() =>{
 
 
     const [data, setData]=useState([])
     const [loading, setLoading] = useState(true)
     //const {id } = useParams()
+    const fechaHoy = new Date() 
 
 
     useEffect(() => {
         const db = getFirestore()
         const reunionesCollection = db.collection('reuniones')
-        const query = reunionesCollection.get()
+        const query = reunionesCollection.where('fecha', '>=', fechaHoy).get()
 
         query.then((respuesta) => {
             setData(respuesta.docs.map((doc)=> 
-                ({...doc.data(), id: doc.id,fecha: doc.data().fecha.toDate().toISOString()})
+                ({...doc.data(), id: doc.id, fecha: doc.data().fecha.toDate().toISOString()})
             ))
             setLoading(false)    
         })
-        
-        
-    },[])
-    
-//
+  
+        // const query = idReunion ? reunionesCollection.where('idReunion', '==', idReunion) : reunionesCollection
+
+        // query.get().then((querySnap) => {
+        //     if(querySnap.size === 0){
+        //         console.log("no existen reuniones disponibles");
+        //     }
+
+        //     setData(querySnap.docs.map( doc => ({...doc.data(), id: doc.id})));
+        },[])
+        console.log(data)
     return(
-        loading ? <h2>Cargando...</h2>: 
-            <Container className="contenedorCard">
-                <ItemList data={data}/>        
-            </Container>
+        <>    
+            <h1 className="mt-5 text-center">ELIGA LA REUNION A LA CUAL SE QUIERE ANOTAR</h1>        
+            {loading ? <h2>Cargando...</h2>: <ItemList data={data}/>}        
+        </>
     )
 }
 
